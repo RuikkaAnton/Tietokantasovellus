babf7fc2e8536bedda16d48a986fd66781f02d51
RuikkaAnton <115700078+RuikkaAnton@users.noreply.github.com>
2023-09-07 10:21:09 +0300

    ProjectConstant class lisäys

--- Varastonhallinta/Varastonhallinta/ProjectConstant.cs
+++ Varastonhallinta/Varastonhallinta/ProjectConstant.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Varastonhallinta
 {
-    public class Tuote
+    internal class Class1
     {
-
     }
 }
